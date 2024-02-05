fdb712beea885e6e549748e2f1c5cc4dbf57ce80
AhmadWMN <ahmadwnabhan2023@gmail.com>
2024-02-05 19:38:28 +0200

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import '@/app/ui/global.css';
+import { inter } from './ui/fonts';
+import {lusitana} from './ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${lusitana.className} antialiased`}>
+        {children}
+        </body>
+    </html>
+  );
+}
